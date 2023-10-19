f94fcbc9a21c8ac91fa9f8533e575d770d66f4bf
Oleg Shabalov <139128677+OlegShabalov@users.noreply.github.com>
2023-10-19 16:05:26 +0300

    Use HSV representation of colors in selectors

--- ColorDialog.cpp
+++ ColorDialog.cpp
@@ -42,22 +42,18 @@ public:
         , t(t)
     {  }
 
-    void setItputColor(unsigned char r, unsigned char g, unsigned char b) {
-        _ir = r;
-        _ig = g;
-        _ib = b;
-        _pointToColor();
+    void setItputColor(float hue) {
+        _hue = hue;
+        _updateColor();
         update();
     }
 
     void setOutputColor(unsigned char r, unsigned char g, unsigned char b) {
-        _or = r;
-        _og = g;
-        _ob = b;
-        const QColor temp = QColor(_or, _og, _ob).toHsv();
+        _outputColor = QColor(r, g, b);
+        const QColor temp = _outputColor.toHsv();
+        _hue = qMax(temp.hsvHueF(), 0.0);
         _pointX = temp.saturationF();
         _pointY = 1 - temp.valueF();
-        t->set(_or, _og, _ob);
     }
 
 protected:
@@ -73,8 +69,8 @@ protected:
             const float k = 1 - (h / static_cast<float>(height()));
 
             QLinearGradient gradient(QPointF(0, 0), QPointF(width(), 1));
-            gradient.setColorAt(0, QColor(255 * k, 255 * k, 255 * k));
-            gradient.setColorAt(1, QColor(_ir * k, _ig * k, _ib * k));
+            gradient.setColorAt(0, QColor::fromHsvF(_hue, 0, k));
+            gradient.setColorAt(1, QColor::fromHsvF(_hue, 1, k));
 
             QBrush brush(gradient);
             painter.setPen(QPen(brush, 1));
@@ -89,12 +85,12 @@ protected:
 
         if (_pointY < 0.4f - _pointX * 0.3f) {
             painter.setPen(QPen(QColor(70, 70, 70), 2));
-            painter.setBrush(QBrush(QColor(_or, _og, _ob)));
+            painter.setBrush(QBrush(_outputColor));
             painter.drawEllipse(x, y, 20, 20);
         }
         else {
             painter.setPen(QPen(Qt::white, 2));
-            painter.setBrush(QBrush(QColor(_or, _og, _ob)));
+            painter.setBrush(QBrush(_outputColor));
             painter.drawEllipse(x, y, 20, 20);
         }
     }
@@ -105,7 +101,7 @@ protected:
             _pointY = event->pos().y() / static_cast<float>(height());
             _pointX = qBound(0.0f, _pointX, 1.0f);
             _pointY = qBound(0.0f, _pointY, 1.0f);
-            _pointToColor();
+            _updateColor();
             update();
             event->accept();
             return;
@@ -118,34 +114,23 @@ protected:
         _pointY = event->pos().y() / static_cast<float>(height());
         _pointX = qBound(0.0f, _pointX, 1.0f);
         _pointY = qBound(0.0f, _pointY, 1.0f);
-        _pointToColor();
+        _updateColor();
         update();
         event->accept();
     }
 
 private:
-    void _pointToColor() {
-        const float kx = 1 - _pointX;
-        const float ky = 1 - _pointY;
-
-        _or = ((255 - _ir) * kx + _ir) * ky;
-        _og = ((255 - _ig) * kx + _ig) * ky;
-        _ob = ((255 - _ib) * kx + _ib) * ky;
-
-        t->set(_or, _og, _ob);
+    void _updateColor() {
+        _outputColor = QColor::fromHsvF(_hue, _pointX, 1 - _pointY).toRgb();
+        t->set(_outputColor.red(), _outputColor.green(), _outputColor.blue());
     }
 
 private:
     float _pointX;
     float _pointY;
+    float _hue;
 
-    unsigned char _ir;
-    unsigned char _ig;
-    unsigned char _ib;
-
-    unsigned char _or;
-    unsigned char _og;
-    unsigned char _ob;
+    QColor _outputColor;
 
     Test * t;
 };
@@ -165,13 +150,7 @@ public:
     }
 
     void setOutputColor(unsigned char r, unsigned char g, unsigned char b) {
-        const int h = qMax(QColor(r, g, b).toHsv().hsvHue(), 0);
-        _pointY = 1 - (h / 359.0f);
-        const QColor temp = QColor::fromHsv(h, 255, 255);
-        _r = temp.red();
-        _g = temp.green();
-        _b = temp.blue();
-        _colorSelector->setItputColor(_r, _g, _b);
+        _hue = qMax(QColor(r, g, b).toHsv().hsvHueF(), 0.0);
         update();
     }
 
@@ -187,25 +166,25 @@ protected:
         for (int h = 0; h < height(); h += lineSize) {
             const float k = 1 - (h / static_cast<float>(height()));
 
-            const QColor color = QColor::fromHsv(k * 359, 255, 255);
+            const QColor color = QColor::fromHsvF(k, 1, 1);
 
             painter.setPen(QPen(color, lineSize));
-            painter.drawRect(0+6, h, width()-12, lineSize);
+            painter.drawRect(0 + 6, h, width() - 12, lineSize);
         }
 
-        const int x = width()  * 0.5f    - 10;
-        const int y = height() * _pointY - 10;
+        const int x = width()  * 0.5f          - 10;
+        const int y = height() * (1.0f - _hue) - 10;
 
         painter.setPen(QPen(QColor(70, 70, 70), 2));
-        painter.setBrush(QBrush(QColor(_r, _g, _b)));
+        painter.setBrush(QBrush(QColor::fromHsvF(_hue, 1, 1)));
         painter.drawEllipse(x, y, 20, 20);
     }
 
     void mousePressEvent(QMouseEvent * event) override {
         if (event->button() == Qt::LeftButton) {
-            _pointY = event->pos().y() / static_cast<float>(height());
-            _pointY = qBound(0.0f, _pointY, 1.0f);
-            _pointToColor();
+            _hue = 1.0f - (event->pos().y() / static_cast<float>(height()));
+            _hue = qBound(0.0f, _hue, 1.0f);
+            _colorSelector->setItputColor(_hue);
             update();
             event->accept();
             return;
@@ -214,29 +193,15 @@ protected:
     }
 
     void mouseMoveEvent(QMouseEvent * event) override {
-        _pointY = event->pos().y() / static_cast<float>(height());
-        _pointY = qBound(0.0f, _pointY, 1.0f);
-        _pointToColor();
+        _hue = 1.0f - (event->pos().y() / static_cast<float>(height()));
+        _hue = qBound(0.0f, _hue, 1.0f);
+        _colorSelector->setItputColor(_hue);
         update();
         event->accept();
     }
 
 private:
-    void _pointToColor() {
-        const QColor c = QColor::fromHsv((1-_pointY) * 359, 255, 255).toRgb();
-        _r = c.red();
-        _g = c.green();
-        _b = c.blue();
-        _colorSelector->setItputColor(_r, _g, _b);
-    }
-
-private:
-    float _pointY;
-
-    unsigned char _r;
-    unsigned char _g;
-    unsigned char _b;
-
+    float _hue;
     ColorSelector * _colorSelector;
 };
 
@@ -263,7 +228,7 @@ ColorDialog::ColorDialog(const QColor & initial, QWidget * parent)
 
     l->addWidget(t);
 
-    //const QColor color(255, 255, 32);
+    //const QColor color(255, 255, 32); // yellow
     //const QColor color(0, 0, 0); // black
     //const QColor color(255, 255, 255); // white
     //const QColor color(255, 0, 0); // red
@@ -272,6 +237,7 @@ ColorDialog::ColorDialog(const QColor & initial, QWidget * parent)
 
     hueSelector->setOutputColor(color.red(), color.green(), color.blue());
     colorSelector->setOutputColor(color.red(), color.green(), color.blue());
+    t->set(color.red(), color.green(), color.blue());
 }
 
 QColor ColorDialog::currentColor() const {
